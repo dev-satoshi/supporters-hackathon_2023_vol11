cddcbafc8216f32762d9db6ca4c4a0eef1a7fae2
kakikuu <s2222055@stu.musashino-u.ac.jp>
2023-11-05 11:19:45 +0900

    update:リッチメニューにタブを追加

--- create.js
+++ create.js
@@ -12,17 +12,120 @@ const client = new line.Client(CONFIG);
 // リッチメニュー全体のサイズ
 const WIDTH = 2500;
 const HEIGHT = 1686;
+const TABHEIGHT = HEIGHT * 0.09 // 左のタブ
+
+// // 1ページ目
+// client.createRichMenu({
+//     "size": {
+//         "width": WIDTH,
+//         "height": HEIGHT
+//     },
+//     "selected": true,
+//     "name": "tab_2",
+//     "chatBarText": "課題一覧",
+//     "areas": [
+//         // {
+//         //     "bounds": {
+//         //         "x": 0,
+//         //         "y": 0,
+//         //         "width": WIDTH / 2,
+//         //         "height": TABHEIGHT
+//         //     },
+//         //     "action": {
+//         //         "type": "richmenuswitch",
+//         //         "richMenuAliasId": "richmenu-alias-b", //後で設定する
+//         //         "data": "richmenu-changed-to-b"
+//         //     },
+//         // },
+//         {
+//             "bounds": {
+//                 "x": (WIDTH / 2) * 1,
+//                 "y": 0,
+//                 "width": WIDTH / 2,
+//                 "height": TABHEIGHT
+//             },
+//             "action": {
+//                 "type": "richmenuswitch",
+//                 "richMenuAliasId": "richmenu-alias-b", //後で設定する
+//                 "data": "richmenu-changed-to-b"
+//             },
+//         },
+//         {
+//             "bounds": {
+//                 "x": 0,
+//                 "y": TABHEIGHT,
+//                 "width": WIDTH / 2,
+//                 "height": HEIGHT
+//             },
+//             "action": {
+//                 "type": "message",
+//                 "text": "課題の登録"
+//             },
+//         },
+//         {
+//             "bounds": {
+//                 "x": (WIDTH / 2) * 1,
+//                 "y": TABHEIGHT,
+//                 "width": WIDTH / 2,
+//                 "height": HEIGHT / 2
+//             },
+//             "action": {
+//                 "type": "message",
+//                 "text": "課題一覧の表示"
+//             },
+//         },
+//         {
+//             "bounds": {
+//                 "x": (WIDTH / 2) * 1,
+//                 "y": TABHEIGHT + (HEIGHT / 2) * 1,
+//                 "width": WIDTH / 2,
+//                 "height": HEIGHT / 2
+//             },
+//             "action": {
+//                 "type": "message",
+//                 "text": "提出率の確認"
+//             },
+//         },
+//     ]
+// })
 
 
+// 2ページ目
 client.createRichMenu({
     "size": {
         "width": WIDTH,
         "height": HEIGHT
     },
     "selected": true,
-    "name": "テスト",
+    "name": "tab_2",
     "chatBarText": "課題一覧",
     "areas": [
+        {
+            "bounds": {
+                "x": 0,
+                "y": 0,
+                "width": WIDTH / 2,
+                "height": TABHEIGHT
+            },
+            "action": {
+                "type": "richmenuswitch",
+                "richMenuAliasId": "richmenu-alias-a", //後で設定する
+                "data": "richmenu-changed-to-a"
+            },
+        },
+        // {
+        //     "bounds": {
+        //         "x": (WIDTH / 2) * 1,
+        //         "y": 0,
+        //         "width": WIDTH / 2,
+        //         "height": TABHEIGHT
+        //     },
+        //     "action": {
+        //         "type": "richmenuswitch",
+        //         "richMenuAliasId": "richmenu-alias-b", //後で設定する
+        //         "data": "richmenu-changed-to-b"
+        //     },
+        // },
         {
             "bounds": {
                 "x": 0,
@@ -32,7 +135,7 @@ client.createRichMenu({
             },
             "action": {
                 "type": "message",
-                "text": "課題の登録"
+                "text": "学生証の表示"
             },
         },
         {
@@ -44,13 +147,15 @@ client.createRichMenu({
             },
             "action": {
                 "type": "message",
-                "text": "エリア2"
+                "text": "クーポンの表示"
             },
         },
     ]
 })
+
+
     .then(async (richMenuId) => {
-        const buffer = await fs.createReadStream('./image_02.png');
+        const buffer = await fs.createReadStream('./image_03.png');
         await client.setRichMenuImage(richMenuId, buffer);
         await client.setDefaultRichMenu(richMenuId);
     })
